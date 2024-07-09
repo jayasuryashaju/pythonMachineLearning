cf6a5bbb2707f78d0c4486a8ee8da5966449aea9
jayasuryashaju <jayasuryashaju3031@gmail.com>
2024-07-09 08:25:01 +0530

    python modules - 1. numpy

---
+++ demo.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+a = np.array([1, 2, 3, 4, 5])
+
+print(a)
+
+
+"""
+array
+    collection of homogeneous data.
+
+
+array created using numpy is called ndimantional array
+
+dimension 
+    number of axis is called dimension
+    
+1D:
+    only x axis
+2D
+    2 ax ices - x axis and y axis
+3D
+    3 ax ices - z axis, x axis, y axis
+    
+order of a matrix
+    number of rows and numbers of columns (2 x 4)
+
+[3
+4
+5
+6]
+order of this matrix :- (4, ) because it is a one dimensional matrix
+
+[1 2 3 4]
+[5 6 7 8]
+[9 1 2 3]
+dimension of this matrix :- (3 X 4) because it is a 2D matrix.
+
+
+
+"""
